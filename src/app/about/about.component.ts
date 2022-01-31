b3152e9436cdbcdb89a0cea03adafb28bef731ad
SpencerOkamoto <35554481+SpencerOkamoto@users.noreply.github.com>
2022-01-30 19:55:59 -0800

    Styling and CSS Grid

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
   name = "Spencer Okamoto";
+  medecc = "Front-End and User Interface | Covid-19 Telemedicine Web App"
+  salton = "Python and Independent Research | San Andreas Fault Behaviors and Trends in Southern California";
 
   constructor() { }
 
