a9a79e1d31412dc916beec09bc9e93b55eaec7e0
SpencerOkamoto <35554481+SpencerOkamoto@users.noreply.github.com>
2022-02-25 09:34:28 -0800

    Redesign A

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -6,11 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
-  name = "Spencer Okamoto";
-  medecc = "Front-End and User Interface | Covid-19 Telemedicine Web App"
-  salton = "Python and Independent Research | San Andreas Fault Behaviors and Trends in Southern California";
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
